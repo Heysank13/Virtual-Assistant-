876195698da306306d26b6ca9c27730e0c964c89
Sanket Girhepunje <121815594+Heysank13@users.noreply.github.com>
2025-07-24 23:53:53 +0530

    Update UserContext.jsx

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -4,7 +4,7 @@ export const userDataContext=createContext()
 
 
 function UserContext({children}) {
-    const serverUrl="http://localhost:8000"
+    const serverUrl="https://virtual-assistant-backend-j4j3.onrender.com"
     const [userData,setUserData]=useState(null)
     const [frontendImage,setFrontendImage]=useState(null)
     const [backendImage,setBackendImage]=useState(null)
